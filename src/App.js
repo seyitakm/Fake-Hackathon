49edfb9852bc511c23bee5fb867d5855b483c0e6
catab1e <tilektursunbaiuulu@gmail.com>
2022-08-15 11:44:48 +0600

    added cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Navbar from "./components/Navbar";
 import MainRoutes from "./MainRoutes";
+import Cards from "./components/Cards";
 
 function App() {
-  return <MainRoutes />;
+  return (
+    <>
+      <MainRoutes />
+      <Navbar />
+      <Cards />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
+import gayporn from "../assets/gays/gayporn.mp4";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="appbar">
+      <div className="inner__appbar">
+        <div className="inner__logo">
+          <h1>NIGHT-CITY</h1>
+        </div>
+        <div className="inner__right">
+          <RouterLink to="/latest">Latest</RouterLink>
+          <RouterLink to="/store">Store</RouterLink>
+          <RouterLink to="/subs">Subscriptions</RouterLink>
+          <RouterLink to="/contacts">Contacts</RouterLink>
+        </div>
+        <div className="auth">
+          <RouterLink to="/signin">Sign up</RouterLink>
+          <RouterLink className="signup" to="/signup">Sign in</RouterLink>
+        </div>
+      </div>
+      <video src={gayporn} loop muted autoPlay></video>
+    </div>
+  );
+};
+
+export default Navbar;
