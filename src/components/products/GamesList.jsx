12623c64a9ff1dd68db308e5b92a6f5478905057
kgklyn <azazaklyn@gmail.com>
2022-08-16 10:04:25 +0300

    pagination

--- src/components/products/GamesList.jsx
+++ src/components/products/GamesList.jsx
@@ -1,31 +1,69 @@
-import { Box } from "@mui/system";
-import React, { useEffect } from "react";
+import { Box, Pagination } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import { useProducts } from "../../contexts/ProductContextProvider";
 import GamesCard from "./GamesCard";
 
 const GamesList = () => {
   const { products, getGames } = useProducts();
-  console.log(products);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams);
+
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 5;
+  const count = Math.ceil(products.length / itemsPerPage);
 
   useEffect(() => {
     getGames();
   }, []);
+
+  useEffect(() => {
+    getGames();
+    setPage(1);
+  }, [searchParams]);
+
+  const handlePage = (e, p) => {
+    console.log(p);
+    setPage(p);
+  };
+
+  function currentData() {
+    const begin = (page - 1) * itemsPerPage;
+    const end = begin + itemsPerPage;
+    return products.slice(begin, end);
+  }
+
   return (
-    <Box sx={{ display: "flex", justifyContent: "center" }}>
+    <Box
+      sx={{
+        display: "flex",
+        mt: 2,
+        flexDirection: "column",
+      }}
+    >
       <Box
         sx={{
           display: "flex",
           justifyContent: "space-around",
-          width: "80vw",
           flexWrap: "wrap",
         }}
       >
         {products ? (
-          products.map((item) => <GamesCard item={item} key={item.id} />)
+          currentData().map((item) => <GamesCard item={item} key={item.id} />)
         ) : (
-          <h2>Loading</h2>
+          <h3>Loading...</h3>
         )}
       </Box>
+
+      <Pagination
+        variant="outlined"
+        shape="rounded"
+        sx={{ m: "0 auto" }}
+        count={count}
+        page={page}
+        onChange={handlePage}
+      />
     </Box>
   );
 };
