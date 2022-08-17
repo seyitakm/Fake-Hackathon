195820b2a6a8211a0dd12b8e40a4dc541c3750b6
seyitakm <seyit200020@gmail.com>
2022-08-17 16:58:49 +0600

    more

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Footer from "../components/Footer";
+import Card from "../components/paymentform/Card";
+import GamesList from "../components/products/GamesList";
+import ProductsPage from "./ProductsPage";
 
 const HomePage = () => {
   return (
     <div>
+      <div className="homePage">
+        <GamesList />
+      </div>
       <Footer />
     </div>
   );
