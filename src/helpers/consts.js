5692423b28c013ddaf1a93df5e8377dcbe626735
kgklyn <azazaklyn@gmail.com>
2022-08-17 10:30:20 +0300

    add cart

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -3,4 +3,8 @@ export const ACTIONS = {
   GET_PRODUCT_DETAILS: "GET_PRODUCT_DETAILS",
 };
 
+export const CART = {
+  GET_CART: "GET_CART",
+  GET_CART_LENGTH: "GET_CART_LENGTH",
+};
 export const JSON_API_PRODUCTS = "http://localhost:8000/product";
---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Cart from "../components/Cart";
+
+const CartPage = () => {
+  return (
+    <div style={{ marginTop: "2%" }}>
+      <Cart />
+    </div>
+  );
+};
+
+export default CartPage;
