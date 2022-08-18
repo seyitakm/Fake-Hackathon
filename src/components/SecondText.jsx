3803bf48d3f085af62de779f208eb275d72a8897
seyitakm <seyit200020@gmail.com>
2022-08-19 01:55:24 +0600

    added text

---
+++ src/components/SecondText.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./SecondText.css";
+const SecondText = () => {
+  return (
+    <div className="body2">
+      <h1 class="jt --debug">
+        <span class="jt__row">
+          <span class="jt__text">Night City</span>
+        </span>
+        <span class="jt__row jt__row--sibling" aria-hidden="true">
+          <span class="jt__text">Night City</span>
+        </span>
+        <span class="jt__row jt__row--sibling" aria-hidden="true">
+          <span class="jt__text">Night City</span>
+        </span>
+        <span class="jt__row jt__row--sibling" aria-hidden="true">
+          <span class="jt__text">Night City</span>
+        </span>
+      </h1>
+    </div>
+  );
+};
+
+export default SecondText;
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,12 +1,14 @@
 import { Box } from "@mui/system";
 import React from "react";
 import Footer from "../components/Footer";
+import MainText from "../components/MainText";
 import GamesList from "../components/products/GamesList";
 import SearchBar from "../components/SearchBar";
 
 const ProductsPage = () => {
   return (
     <Box>
+      <MainText />
       <SearchBar />
       <div className="homePageMain">
         <div className="homePage">
