d408d31914960b7773e58484a5dcd912e4ecde7f
kgklyn <azazaklyn@gmail.com>
2022-08-18 14:46:44 +0300

    add card

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -29,24 +29,6 @@ const Footer = () => {
             </ul>
           </div>
 
-          <div className="col2">
-            <h3 className="heading">Language</h3>
-            <div className="languages">
-              <a href="#">Deutsch</a>
-              <a href="#">English</a>
-              <a href="#">Espaṅol</a>
-              <a href="#">Français</a>
-              <a href="#">Indonesian</a>
-              <a href="#">Italian</a>
-              <a href="#">Nederlands</a>
-              <a href="#">Polnisch</a>
-              <a href="#">Português</a>
-              <a href="#">Русский</a>
-              <a href="#">Tiéng Viêt</a>
-              <a href="#">Türkçe</a>
-            </div>
-          </div>
-
           <div className="col3">
             <h3 className="heading">Get in touch</h3>
             <div className="social">
